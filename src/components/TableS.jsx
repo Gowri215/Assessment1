680632a933971d9dc04adc1c63a29813fe52fe61
Nevin <nevinthomas800@gmail.com>
2023-05-27 07:48:45 +0530

    First

---
+++ src/components/TableS.jsx
@@ -0,0 +1,71 @@
+import { Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const TableS = () => {
+    const [datas,setData] = useState([])
+
+    useEffect(() =>{
+
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then((response) =>{
+            setData(response.data)
+
+        })
+        .catch((error) =>{
+            console.log(error)
+        })
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+  return (
+    <div>
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell>TITLE</TableCell>
+
+
+                    </TableRow>
+
+
+                </TableHead>
+
+                <TableBody>
+                    {datas.map((val,index)=>{
+                        return(
+                            <TableRow key={index}>
+
+                            <TableCell>{val.id}</TableCell>
+                            <TableCell>{val.title}</TableCell>
+
+
+
+                        </TableRow>
+                         )
+
+                    })}
+
+
+                </TableBody>
+    </Table>
+
+</TableContainer>
+
+    </div>
+  )
+}
+
+export default TableS
